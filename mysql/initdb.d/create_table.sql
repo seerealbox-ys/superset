b29c3caebdf6b618f9f222593d9cff975864694b
yys431 <yys431@seerealbox.com>
2023-05-03 18:05:29 +0900

    테이블추가

--- mysql/initdb.d/create_table.sql
+++ mysql/initdb.d/create_table.sql
@@ -1,7 +1,17 @@
 create table test_db.user_actions
 (
+    id                      VARCHAR(24) PRIMARY KEY,
     timestamp               datetime,
     screenName              varchar(32),
     user_id                 int,
-    mode                    varchar(32)
-)
+    mode                    varchar(32),
+    session                     varchar(32)
+);
+create table test_db.usage_screen
+    (
+        id                      VARCHAR(24) PRIMARY KEY,
+        timestamp               datetime,
+        screenName              varchar(32),
+        user_id                 int,
+        usage                   datetime        
+    );
