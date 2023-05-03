fdc84ed222d829f1f039a6571087b6eec4fb2bde
yys431 <yys431@seerealbox.com>
2023-05-03 10:34:28 +0900

    init

---
+++ mysql/initdb.d/create_table.sql
@@ -0,0 +1,7 @@
+create table test_db.user_actions
+(
+    timestamp               datetime,
+    screenName              varchar(32),
+    user_id                 int,
+    mode                    varchar(32)
+)
